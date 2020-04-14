cb91e8454b81d1902844cf3a83ac68d18835f6a2
WILLIAM INTAN <wintan@student.unimelb.edu.au>
2020-04-14 21:09:41 +1000

    copy week4

--- routes/my-routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const myRoutes = express.Router();
-
-//const authorController = require("../controllers")
-
-myRoutes.get("/", (req, res) => res.send('<H1>System</H1>'));
-
-module.exports = myRoutes;
