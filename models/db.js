b4d541696b5d84b83f010a1cee8cf8e1ba88d722
WILLIAM INTAN <wintan@student.unimelb.edu.au>
2020-04-14 21:34:05 +1000

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-const uri = "mongodb+srv://wintan:<password>@cluster0-d2k2z.mongodb.net/test?retryWrites=true&w=majority";
+const uri = "mongodb+srv://wintan:123@cluster0-d2k2z.mongodb.net/test?retryWrites=true&w=majority";
 
 mongoose.connect(uri,
     function(err){
