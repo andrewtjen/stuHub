631fb3f29d559ad4e410996e2835b47fdb880e58
nikolasleand <61858245+nikolasleand@users.noreply.github.com>
2020-05-14 21:13:37 +1000

    bootstrap

--- views/modal1.js
+++ views/modal1.js
@@ -1,28 +1,3 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
-
-openModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = document.querySelector(button.dataset.modalTarget)
-        openModal(modal)
-    })
-})
-
-overlay.addEventListener('click', () => {
-    const modals = document.querySelectorAll('.modal.active')
-    modals.forEach(modal => {
-        closeModal(modal)
-    })
-})
-
-closeModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = button.closest('.modal')
-        closeModal(modal)
-    })
-})
-
 function openModal() {
     // document.getElementById("modal").style.height = '500px';
     // document.getElementById("modal").style.width = '500px';
@@ -30,7 +5,7 @@ function openModal() {
     const overlay = document.getElementById("overlay")
     modal.style.transform = "translate(-50%, -50%) scale(1)";
     overlay.style.opacity = "0.6";
-    overlay.style.pointerEvents = "all";
+    //overlay.style.pointerEvents = "all";
     //document.getElementById("modal").style.display = 'block';
     //if (modal == null) return
     // modal.classList.add('active')
@@ -46,7 +21,7 @@ function closeModal() {
     const overlay = document.getElementById("overlay")
     modal.style.transform = "translate(-50%, -50%) scale(0)";
     overlay.style.opacity = "0";
-    overlay.style.pointerEvents = "none";
+    //overlay.style.pointerEvents = "none";
     // modal.classList.remove('active')
     // overlay.classList.remove('active')
 }
